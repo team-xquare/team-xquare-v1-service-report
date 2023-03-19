3e36545f5e99f6e5f2adbb1833df757554baab4c
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-19 23:31:33 +0900

    ⚡️ :: koin 블록 등록

--- src/main/kotlin/xquare/com/Application.kt
+++ src/main/kotlin/xquare/com/Application.kt
@@ -1,15 +1,29 @@
 package xquare.com
 
 import io.ktor.server.application.*
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
 import xquare.com.infrastructure.database.config.DatabaseConfig
+import xquare.com.infrastructure.module.reportModule
 import xquare.com.plugins.*
+import xquare.com.presentation.api.reportRouting
 
-fun main(args: Array<String>): Unit =
+fun main(args: Array<String>) {
+    stopKoin()
+    startKoin {
+        val modules = arrayOf(reportModule)
+
+        modules(*modules)
+    }
+
     io.ktor.server.netty.EngineMain.main(args)
+}
+
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     DatabaseConfig.init()
+    reportRouting()
     configureSerialization()
     configureDatabases()
     configureHTTP()
