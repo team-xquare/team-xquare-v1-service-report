cc69663d8a57bb53fa632666f8d18e060dc8bc0a
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-19 23:26:37 +0900

    ⚡️ :: 신고 도메인 nullable 수정

--- src/main/kotlin/xquare/com/domain/report/Report.kt
+++ src/main/kotlin/xquare/com/domain/report/Report.kt
@@ -4,10 +4,10 @@ import java.time.LocalDateTime
 import java.util.UUID
 
 data class Report(
-    val id: UUID,
+    val id: UUID?,
     val userId: UUID,
     val reason: String,
     val category: Category,
     val imageUrl: String?,
-    val createdAt: LocalDateTime,
+    val createdAt: LocalDateTime?,
 )
