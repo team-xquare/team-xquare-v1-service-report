749799b748ff0fa47c0930e0be09b90effc6b6c1
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-19 23:25:18 +0900

    📑 :: 신고 레포지토리 추가

---
+++ src/main/kotlin/xquare/com/infrastructure/database/ReportRepository.kt
@@ -0,0 +1,23 @@
+package xquare.com.infrastructure.database
+
+import java.time.LocalDateTime
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.transactions.transaction
+import xquare.com.domain.report.Report
+import xquare.com.infrastructure.database.config.DatabaseConfig.query
+import xquare.com.infrastructure.database.entity.ReportEntity
+
+class ReportRepository {
+
+    suspend fun save(report: Report) = query {
+        transaction {
+            ReportEntity.insert {
+                it[userId] = report.userId
+                it[reason] = report.reason
+                it[category] = report.category
+                it[imageUrl] = report.imageUrl
+                it[createdAt] = report.createdAt ?: LocalDateTime.now()
+            }
+        }
+    }
+}
