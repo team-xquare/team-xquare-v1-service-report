c7de869cd3743f93f4a58fbe4fa1cbb2e23b35ec
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-19 22:29:27 +0900

    📑 :: 데이터베이스 설정 추가

---
+++ src/main/kotlin/xquare/com/infrastructure/database/config/DatabaseConfig.kt
@@ -0,0 +1,26 @@
+package xquare.com.infrastructure.database.config
+
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import org.jetbrains.exposed.sql.transactions.transaction
+import xquare.com.infrastructure.database.entity.ReportEntity
+
+object DatabaseConfig {
+    fun init() {
+        val url = System.getenv("DB_URL")
+        val driver = System.getenv("DB_DRIVER")
+        val username = System.getenv("DB_USERNAME")
+        val password = System.getenv("DB_PASSWORD")
+
+        Database.connect(url, driver, username, password)
+
+        transaction {
+            SchemaUtils.create(ReportEntity)
+        }
+    }
+
+    suspend fun <T> query(block: suspend () -> T): T =
+        newSuspendedTransaction(Dispatchers.IO) { block() }
+}
