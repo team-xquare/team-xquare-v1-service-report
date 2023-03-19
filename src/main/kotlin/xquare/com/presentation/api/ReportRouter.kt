a4f0fd63b303d9e2178ddcb462cb8b861ed7bae8
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-20 01:41:02 +0900

    ⚡️ :: 신고 response 추가
    header 값 임시 추가

--- src/main/kotlin/xquare/com/presentation/api/ReportRouter.kt
+++ src/main/kotlin/xquare/com/presentation/api/ReportRouter.kt
@@ -1,17 +1,26 @@
 package xquare.com.presentation.api
 
+import com.mysql.cj.xdevapi.Schema
 import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.application.*
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import java.util.UUID
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import org.koin.ktor.ext.inject
 import xquare.com.application.usecase.CreateReportUseCase
+import xquare.com.infrastructure.error.BaseException
 import xquare.com.presentation.api.dto.request.CreateReportRequest
+import xquare.com.presentation.api.dto.response.CreateReportResponse
 
 fun Application.reportRouting() {
     val createReportUseCase: CreateReportUseCase by inject()
@@ -21,13 +30,16 @@ fun Application.reportRouting() {
             post {
                 val request = call.receive<CreateReportRequest>()
                 val userId = call.request.headers["Request-User-Id"]
+                val userRole = call.request.headers["Request-User-Role"]
+                val authority = call.request.headers["Request-User-Authorities"]
 
+                var reportId = UUID(0, 0)
                 runCatching {
-                    createReportUseCase.execute(UUID.fromString(userId), request)
+                    reportId = createReportUseCase.execute(UUID.fromString(userId), request)
                 }.onSuccess {
-                    call.respond(HttpStatusCode.Created, "")
+                    call.respond(HttpStatusCode.Created, CreateReportResponse(reportId.toString()))
                 }.onFailure {
-                    call.respond(HttpStatusCode.InternalServerError, it.message!!)
+                    call.respond(HttpStatusCode.InternalServerError, BaseException())
                     it.printStackTrace()
                 }
             }
