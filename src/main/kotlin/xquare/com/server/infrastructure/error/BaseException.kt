ce16582ab3ddac12539e1bcb1174b0830d264485
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-20 09:53:44 +0900

    🐛 :: @Serializable 추가

--- src/main/kotlin/xquare/com/server/infrastructure/error/BaseException.kt
+++ src/main/kotlin/xquare/com/server/infrastructure/error/BaseException.kt
@@ -1,5 +1,8 @@
 package xquare.com.server.infrastructure.error
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class BaseException(
     val status: Int = 500,
     val message: String = "Internal Server Error"
