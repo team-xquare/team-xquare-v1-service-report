0bb9ed8e9aa3d59ba2c8e0a7d29e5e9a7a264da8
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-20 10:22:08 +0900

    🐛 :: 컬럼 사이즈 수정

--- src/main/kotlin/xquare/com/server/infrastructure/database/entity/ReportEntity.kt
+++ src/main/kotlin/xquare/com/server/infrastructure/database/entity/ReportEntity.kt
@@ -7,7 +7,7 @@ import xquare.com.server.domain.report.Category
 object ReportEntity : UUIDTable("tbl_report") {
     val userId = uuid("user_id")
     val reason = varchar("reason", 500)
-    val category = enumerationByName("category", 10, Category::class)
+    val category = enumerationByName("category", 255, Category::class)
     val imageUrl = varchar("image_url", 255).nullable()
     val createdAt = datetime("created_at")
 }
