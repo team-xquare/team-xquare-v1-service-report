4990bbba156849764fa4c9ff22f582a8cdcfd397
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-20 15:08:13 +0900

    ⚡️ :: 모듈 의존성 주입

--- src/main/kotlin/xquare/com/server/infrastructure/module/ReportModule.kt
+++ src/main/kotlin/xquare/com/server/infrastructure/module/ReportModule.kt
@@ -2,9 +2,11 @@ package xquare.com.server.infrastructure.module
 
 import org.koin.dsl.module
 import xquare.com.server.application.report.usecase.CreateReportUseCase
+import xquare.com.server.infrastructure.database.ReportImageRepository
 import xquare.com.server.infrastructure.database.ReportRepository
 
 val reportModule = module {
     single { ReportRepository() }
-    single { CreateReportUseCase(get()) }
+    single { ReportImageRepository() }
+    single { CreateReportUseCase(get(), get()) }
 }
