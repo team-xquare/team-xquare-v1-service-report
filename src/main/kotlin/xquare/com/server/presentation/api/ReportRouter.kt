044d340ccfbfe029f987caaae61830370e4d5ede
Jeong Yoon <dlwjddbs14@naver.com>
2023-03-22 12:32:52 +0900

    ⚰️ :: runCatching 블록 제거

--- src/main/kotlin/xquare/com/server/presentation/api/ReportRouter.kt
+++ src/main/kotlin/xquare/com/server/presentation/api/ReportRouter.kt
@@ -11,7 +11,6 @@ import io.ktor.server.routing.routing
 import java.util.UUID
 import org.koin.ktor.ext.inject
 import xquare.com.server.application.report.usecase.CreateReportUseCase
-import xquare.com.server.domain.common.exception.BaseException
 import xquare.com.server.presentation.api.dto.request.CreateReportRequest
 import xquare.com.server.presentation.api.dto.response.CreateReportResponse
 
@@ -24,15 +23,9 @@ fun Application.reportRouting() {
                 val userId = call.request.headers["Request-User-Id"]
                 val request = call.receive<CreateReportRequest>()
 
-                var reportId = UUID(0, 0)
-                runCatching {
-                    reportId = createReportUseCase.execute(UUID.fromString(userId), request)
-                }.onSuccess {
-                    call.respond(HttpStatusCode.Created, CreateReportResponse(reportId.toString()))
-                }.onFailure {
-                    call.respond(HttpStatusCode.InternalServerError, BaseException())
-                    it.printStackTrace()
-                }
+                val reportId = createReportUseCase.execute(UUID.fromString(userId), request)
+                
+                call.respond(HttpStatusCode.Created, CreateReportResponse(reportId.toString()))
             }
         }
     }
