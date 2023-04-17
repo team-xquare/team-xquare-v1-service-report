d86473d80deedf091dc3b5ee902ccbc9a89ed247
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-18 00:42:31 +0900

    🔀 :: URL 수정

--- src/main/kotlin/xquare/com/server/presentation/api/ReportRouter.kt
+++ src/main/kotlin/xquare/com/server/presentation/api/ReportRouter.kt
@@ -8,23 +8,23 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
-import java.util.UUID
 import org.koin.ktor.ext.inject
 import xquare.com.server.application.report.usecase.CreateReportUseCase
 import xquare.com.server.presentation.api.dto.request.CreateReportRequest
 import xquare.com.server.presentation.api.dto.response.CreateReportResponse
+import java.util.UUID
 
 fun Application.reportRouting() {
     val createReportUseCase: CreateReportUseCase by inject()
 
     routing {
-        route("/reports") {
+        route("") {
             post {
                 val userId = call.request.headers["Request-User-Id"]
                 val request = call.receive<CreateReportRequest>()
 
                 val reportId = createReportUseCase.execute(UUID.fromString(userId), request)
-                
+
                 call.respond(HttpStatusCode.Created, CreateReportResponse(reportId.toString()))
             }
         }
