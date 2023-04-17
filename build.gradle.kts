0d84058b5a3b32b5179a827f3059424abefdca59
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-18 00:28:27 +0900

    🔀 :: jar enable=false 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,3 +43,7 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
+
+tasks.getByName<Jar>("jar") {
+    enabled = false
+}
